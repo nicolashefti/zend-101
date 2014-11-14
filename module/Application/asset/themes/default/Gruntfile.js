eca8c86c422fc96d943f99cd2c11ec8873e160c7
Nicolas Hefti <n.hefti@traveltrex.com>
2014-11-14 15:15:01 +0100

    Translation in progress...

---
+++ module/Application/asset/themes/default/Gruntfile.js
@@ -0,0 +1,63 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    banner : '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+    options : {
+      dist: '../../../../../public/'
+    },
+    cssmin: {
+      options: {
+        banner : '<%= banner %>',
+      },
+      build: {
+        src: 'scss/style.css',
+        dest: 'css/<%= pkg.name %>.min.js'
+      }
+    },
+    compass: {
+      dist: {
+        options: {
+            config: 'config.rb',
+        }
+      }
+    },
+    copy   : {
+      css: {
+        files: [
+            {
+                expand: true,
+                src    : ['css/*'],
+                dest   : '<%= options.dist %>',
+                filter : 'isFile'
+            },
+        ]
+      },
+    },
+    watch  : {
+      css: {
+          files: ['scss/*.scss'],
+          tasks: ['development-scss']
+      },
+    },
+  });
+
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-copy');
+  
+  // Default tasks
+  grunt.registerTask('default', ['compass', 'copy:css']);
+  
+  // Deployment tasks
+  grunt.registerTask('deployment', ['compass']);
+  
+  // Development tasks, triggered by watch
+  grunt.registerTask('development-scss', ['compass']);
+
+};
---
+++ module/Application/asset/themes/new-theme/config.rb
@@ -0,0 +1,30 @@
+# Set this to the root of your project when deployed:
+# http_path = "../"
+# css_dir = "../../dist/theme/facetravel/css"
+css_dir = "css"
+sass_dir = "scss"
+
+# images_dir = "images"
+# sprite_load_path = "../../shared/images"
+# generated_images_dir = "../../shared/images"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+asset_cache_buster :none
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass scss scss && rm -rf sass && mv scss sass
+
